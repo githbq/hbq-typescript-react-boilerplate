9ddf4bcf7276ca90e1fe389ede36ce87309c45fd
hubiqing <hubiqing01@58ganji.com>
2017-08-18 14:50:56 +0800

    editorconfig

--- src/routes/Demo/index.tsx
+++ src/routes/Demo/index.tsx
@@ -5,11 +5,11 @@ import './index.less'
 import * as React from 'react'
 import DemoComponent from '@/components/Demo'
 export default class extends React.Component {
-    render() {
-        return <div className='demo-route'>
-            this is demo-route  
+  render() {
+    return <div className='demo-route'>
+      this is demo-route
             <DemoComponent />
-        </div>
-    }
+    </div>
+  }
 
-} 
+}
