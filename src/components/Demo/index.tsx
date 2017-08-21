cd83b924d7e2b5cf62a26d78bdffad71fdd1441a
hubiqing <hubiqing01@58ganji.com>
2017-08-21 18:19:39 +0800

    ts-react

--- src/components/Demo/index.tsx
+++ src/components/Demo/index.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react'
 import './style'
 
+interface State {
+  abc: number
+}
+interface Props {
+}
 /**
  * 组件demo
  */
-export default class extends React.PureComponent {
+export default class extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+  }
   render() {
+    this.setState({ abc: 1234 })
     return (
       <div>
         <h1 className='demo-component'>
-          this is demo-component
+          <a href='detail.html'>跳转到详情页面</a>
         </h1>
       </div>
     )
   }
 }
+
+
