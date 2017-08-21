a52f3e63b64489498c327f1b07d4bbd7253a763b
hubiqing <hubiqing01@58ganji.com>
2017-08-21 20:21:55 +0800

    d

--- src/entries/detail.2.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * 程序的入口
- */
-import * as  React from 'react'
-import * as  ReactDOM from 'react-dom'
-
-import Detail from '@/routes/Detail'
-ReactDOM.render(<Detail />, document.getElementById('root'))
