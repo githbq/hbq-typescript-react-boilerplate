1c7724563f10e5bdaa4c693992308e63fa37b1f7
hubiqing <hubiqing01@58ganji.com>
2017-08-22 12:18:33 +0800

    demo1

--- src/routes/Demo/index.tsx
+++ src/routes/Demo/index.tsx
@@ -4,15 +4,24 @@
 import './style'
 import * as React from 'react'
 import DemoComponent from '@/components/Demo'
-interface State {
+export interface State {
   list: Array<any>
 }
-export default class extends React.Component<void, State> {
-
+export interface Props {
+  list: Array<any>
+}
+export default class extends React.Component<Props, State> {
+  state = { list: [1, 2, 34, 34] }
+  static defaultProps = {
+    list: [6, 7, 87, 8, 4]
+  }
   render() {
+    console.log(this.state)
     return <div className='demo-route'>
       this is demo-route
              <DemoComponent />
+      {this.state.list.join('=-=')}
+      {this.props.list.join('****')}
     </div>
   }
 
