096359bcf312c1bc2d3644401a9be1e217404370
hubiqing <hubiqing01@58ganji.com>
2017-09-06 12:11:30 +0800

    ok

--- src/entries/xx/detail.tsx
+++ src/entries/xx/detail.tsx
@@ -5,4 +5,4 @@ import * as  React from 'react'
 import * as  ReactDOM from 'react-dom'
 
 import Detail from '@/routes/Detail'
-ReactDOM.render(<div>11111</div>, document.getElementById('root'))
+ReactDOM.render(<Detail/>, document.getElementById('root'))
