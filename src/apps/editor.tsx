959ccba2e058a0569e38e8ad43052f9e3801c27d
hbq <hbqhbq123@qq.com>
2017-09-09 23:48:17 +0800

    ok

---
+++ src/apps/editor.tsx
@@ -0,0 +1,17 @@
+/**
+ * 程序的入口
+ */
+import * as  React from 'react'
+import * as  ReactDOM from 'react-dom'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+//页面
+import DemoRoute from './routes/Demo'
+// 路由表
+const routes = (
+  <Router history={hashHistory}>
+    {/*<IndexRoute component={DemoRoute} />*/}
+    <Route path='/' component={DemoRoute} />
+  </Router>
+)
+ReactDOM.render(routes, document.getElementById('root'))
+
--- src/routes/Demo/index.tsx
+++ src/routes/Demo/index.tsx
@@ -5,38 +5,13 @@ import * as styles from './style.less'
 import * as React from 'react'
 import * as classNames from 'classnames'
 
-import { newState } from '@/models/editor'
-import { observer } from 'mobx-react'
-
-interface Props {
-  value: string
-  onChange?: React.ChangeEventHandler<HTMLTextAreaElement>
-  title: string
-}
-class TextArea extends React.Component<Props, {}> {
-  render() {
-    return <div className={classNames(styles['text-area-panel'], styles['textarea-container'])}>
-      <div className={styles['title']}>{this.props.title}</div>
-      <textarea onChange={this.props.onChange} value={this.props.value} className={styles['text-area-big']}></textarea>
-    </div>
-  }
-}
-
-@observer
 export default class extends React.Component {
-  onChange = (e) => {
-    newState.convert(e.target.value)
-  }
+
   render() {
     return <div className={styles['demo-route']}>
-      <div className={classNames('container', styles['container-head'])}>
-        <h1>即时url参数解析</h1>
-      </div>
-      <div className={classNames('container', styles['container-body'])}>
-        <TextArea title='原码' onChange={this.onChange} value={newState.content} />
-        <TextArea title='解码结果' value={newState.contentResult} />
-        <TextArea title='参数结果' value={newState.jsonResult} />
-      </div>
+      welcome to jsfan.cn!
+      <br />
+      <a href='./editor.html'>跳转到url解析</a>
     </div>
   }
 
