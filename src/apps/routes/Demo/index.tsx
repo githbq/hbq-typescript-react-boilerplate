161afc69df33ea3a30e2b026356b80f541be1300
hbq <hbqhbq123@qq.com>
2017-09-10 11:26:02 +0800

    copy

--- src/apps/routes/Demo/index.tsx
+++ src/apps/routes/Demo/index.tsx
@@ -8,17 +8,42 @@ import * as classNames from 'classnames'
 import { newState } from '@/models/editor'
 import { observer } from 'mobx-react'
 
+import * as CopyToClipboard from 'react-copy-to-clipboard'
+
 interface Props {
   value: string
   onChange?: React.ChangeEventHandler<HTMLTextAreaElement>
   title: string
 }
-class TextArea extends React.Component<Props, {}> {
+interface State {
+  copyed: boolean
+}
+class TextArea extends React.Component<Props, State> {
+
+  state = {
+    copyed: false
+  }
+  componentWillReceiveProps(newProps) {
+    this.setState({ copyed: false })
+  }
+  onCopyed = () => {
+    this.setState({ copyed: true })
+  }
   render() {
     return <div className={classNames(styles['text-area-panel'], styles['textarea-container'])}>
-      <div className={styles['title']}>{this.props.title}</div>
+      <div className={styles['head-container']}>
+        <div className={styles['title']}>{this.props.title}</div>
+        <div className={styles['btn-container']}>
+          <CopyToClipboard text={this.props.value} onCopy={this.onCopyed}>
+            <div className={classNames({ [styles['btn-copy']]: true, [styles['ok']]: this.state.copyed })}>
+              {this.state.copyed ? '已复制!' : '复制'}
+            </div>
+          </CopyToClipboard>
+        </div>
+      </div>
+
       <textarea onChange={this.props.onChange} value={this.props.value} className={styles['text-area-big']}></textarea>
-    </div>
+    </div >
   }
 }
 
@@ -33,7 +58,7 @@ export default class extends React.Component {
         <h1>即时url参数解析</h1>
       </div>
       <div className={classNames('container', styles['container-body'])}>
-        <TextArea title='原码' onChange={this.onChange} value={newState.content} />
+        <TextArea title='url 或者 formData' onChange={this.onChange} value={newState.content} />
         <TextArea title='解码结果' value={newState.contentResult} />
         <TextArea title='参数结果' value={newState.jsonResult} />
       </div>
