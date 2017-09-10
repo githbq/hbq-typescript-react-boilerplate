339ee6db75903dfb9cb1992c238a97d075cb92b2
hbq <hbqhbq123@qq.com>
2017-09-10 22:14:48 +0800

    rimrafOther

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ajax_1 = require("./ajax");
-var lazyload_1 = require("./lazyload");
-var momentHelper_1 = require("./momentHelper");
-exports.default = { ajax: ajax_1.default, lazyload: lazyload_1.default, momentHelper: momentHelper_1.default };
-//# sourceMappingURL=index.js.map
