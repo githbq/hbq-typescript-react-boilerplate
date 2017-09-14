5550d61577d6b0854e05954a2fb5dccbcb721ee6
hubiqing <hubiqing01@58ganji.com>
2017-09-14 20:39:17 +0800

    ok

--- dev-config/configs/globalConfig.js
+++ dev-config/configs/globalConfig.js
@@ -17,19 +17,3 @@ module.exports = {
   favicon: '/assets/favicon.ico',
   isWebpackDevServer: isWebpackDevServer()
 }
-
-
-
-
-
-module.exports = {
-  htmlPaths: htmlPaths.filter(n => !/index\.(pug|html)/.test(n)),
-  title: 'react-boilerplate',
-  apiUrl: `http://localhost:3604`, //如果配了这个  就会走后端的反向代理
-  favicon: '/assets/favicon.ico',
-  isWebpackDevServer: isWebpackDevServer()
-}
-
-function isWebpackDevServer() {
-  return process.argv[1] && !!(/webpack-dev-server/.exec(process.argv[1]))
-}
