9cca80f8c58b2409f244c9b014ceff928bf5cf32
hbq <hbqhbq123@qq.com>
2017-09-15 22:08:01 +0800

    ok

--- dev-config/configs/globalConfig.js
+++ dev-config/configs/globalConfig.js
@@ -1,12 +1,11 @@
 /**
  * 全局配置
  */
-const pathTool = require('path')
 const { templateObject } = require('./entry')
 function isWebpackDevServer() {
   return process.argv[1] && !!(/webpack-dev-server/.exec(process.argv[1]))
 }
-const { APPS_PATH, __DEV__, NODE_ENV } = require('./constants')
+const { APPS_PATH, __DEV__, NODE_ENV, pathTool } = require('./constants')
 const srcRelative = pathTool.relative.bind(pathTool, APPS_PATH)
 const htmlPaths = Object.keys(templateObject).map(n =>
   srcRelative(templateObject[n]).replace('.pug', '.html')
