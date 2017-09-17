4b96023ba2a2471c0153319a09fbd9a00d51bbd6
hbq <hbqhbq123@qq.com>
2017-09-17 18:28:28 +0800

    ok

--- src/routes/Demo/index.tsx
+++ src/routes/Demo/index.tsx
@@ -20,7 +20,7 @@ export default class extends React.Component<Props, State> {
     console.log(this.state)
     return <div className= {styles['demo-route']}>
       <input type='text'/>
-      this is demo-route  1111 88
+      this is demo-route  1111 88222111222333
              <DemoComponent />
       {this.state.list.join('---')}
       {this.props.list.join('****')}
