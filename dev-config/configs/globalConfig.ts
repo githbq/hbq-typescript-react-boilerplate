826c9350b5640ddc187bf9d8b35788ce1609698a
hubiqing <hubiqing01@58ganji.com>
2017-09-18 13:46:52 +0800

    ok

--- dev-config/configs/globalConfig.ts
+++ dev-config/configs/globalConfig.ts
@@ -1,14 +1,14 @@
 /**
  * 全局配置
  */
-import { templateObject } from './entry'
+import { entryObject } from './entry'
 
 import { APPS_PATH, __DEV__, NODE_ENV, pathTool, isWebpackDevServer } from './constants'
 const srcRelative = pathTool.relative.bind(pathTool, APPS_PATH)
-const htmlPaths = Object.keys(templateObject).map(n => srcRelative(templateObject[n]))
+const htmlPaths = Object.keys(entryObject).map(n => srcRelative(entryObject[n]) + '.html')
 
 export default {
-  htmlPaths: htmlPaths.filter(n => !/index\.(pug|html)/.test(n)),
+  htmlPaths: htmlPaths,
   title: 'ts-react-boilerplate',
   apiUrl: `http://localhost:3604`, //如果配了这个  就会走后端的反向代理
   favicon: '/assets/favicon.ico',
--- dev-config/configs/plugins.ts
+++ dev-config/configs/plugins.ts
@@ -24,6 +24,21 @@ let _plugins = [
     'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
     '__DEV__': JSON.stringify(__DEV__)
   }),
+  // new ExtractTextPlugin('css/[name].[contenthash:8].css'),
+  new ExtractTextPlugin('css/[name].css'),
+  // split vendor js into its own file
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'common',
+    filename: 'common.js',
+    minChunks: function (module, count) {
+      // any required modules inside node_modules are extracted to vendor
+      return (
+        // module.resource &&
+        // /\.js$/.test(module.resource) &&
+        /node_modules/.test(module.resource)
+      )
+    }
+  }),
   new CopyWebpackPlugin(
     [
       {
@@ -57,21 +72,6 @@ if (__DEV__) {
 } else {
   _plugins = [
     ..._plugins,
-    // new ExtractTextPlugin('css/[name].[contenthash:8].css'),
-    new ExtractTextPlugin('css/[name].css'),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common',
-      filename: 'common.js',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          // module.resource &&
-          // /\.js$/.test(module.resource) &&
-          /node_modules/.test(module.resource)
-        )
-      }
-    }),
     new webpack.optimize.ModuleConcatenationPlugin(),
     // new WebpackMd5Hash(),
     new webpack.HashedModuleIdsPlugin(), //这个比 webpack.NamedModulesPlugin 更有用
