c8ec337bfa334786d8e225b3f7191cf338510fdc
hubiqing <hubiqing01@58ganji.com>
2017-10-16 12:13:19 +0800

    px2rem

--- dev-config/configs/utils.ts
+++ dev-config/configs/utils.ts
@@ -2,9 +2,11 @@
  * 其他配置或工具
  */
 import * as  autoprefixer from 'autoprefixer'
+import * as px2rem from 'postcss-px2rem'
 
 //使用postcss作为默认的CSS编译器
 export const postCSSConfig = [
+  // px2rem({ remUnit: 75 }),
   autoprefixer({
     browsers: [
       '> 5%'
