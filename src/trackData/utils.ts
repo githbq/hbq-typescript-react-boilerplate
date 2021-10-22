815d904f78e4c0219149be2d5f5668281318ab55
hubiqing <hubiqing03@kuaishou.com>
2021-10-22 18:29:17 +0800

    fix trackData.utils

--- src/trackData/utils.ts
+++ src/trackData/utils.ts
@@ -4,15 +4,17 @@ import { track, autoShow } from '@/utils/logger';
 type Args = [string | HTMLElement, (string | any)?, (any | string[])?, string[]?];
 type TrackOptions = {
   trackType: 'PV' | 'SHOW' | 'CUSTOM' | 'CLICK' | 'DRAG' | 'PULL' | 'LONG_PRESS' | 'SCALE';
-  optionsType?: 'OPERATION' | 'enter' | 'hide' | 'visible' | 'leave' | any;
+  optionsType?: 'USER_OPERATION' | 'enter' | 'hide' | 'visible' | 'leave' | any;
 };
 export const createHandler = (commonData: any, usuallyKeys?: string[]) => {
-  let tempParams: any = { value: null };
+  let tempParams: any = null;
   let resolve: any = () => {};
   const ready = new Promise((resolveFn: any) => {
     resolve = resolveFn;
   });
   const core = async (args: Args, trackOptions?: TrackOptions) => {
+    const privateTempParams = tempParams;
+    tempParams = null;
     await ready;
     let dom: HTMLElement | null = null;
     let eventName: string, params: any, dependencies: string[];
@@ -41,13 +43,12 @@ export const createHandler = (commonData: any, usuallyKeys?: string[]) => {
 
         {}
       ),
-      tempParams.value
+      privateTempParams
     );
-    tempParams.value = null;
     if (dom) {
       autoShow(dom, eventName, params);
     } else {
-      const newTrackOptions = trackOptions || { trackType: 'CLICK', optionsType: 'OPERATION' };
+      const newTrackOptions = trackOptions || { trackType: 'CLICK', optionsType: 'USER_OPERATION' };
       if (newTrackOptions.trackType === 'PV') {
         track(newTrackOptions.trackType, {
           page: eventName,
@@ -66,7 +67,7 @@ export const createHandler = (commonData: any, usuallyKeys?: string[]) => {
   const methods = {
     ready: resolve,
     setTempParams(params: any) {
-      tempParams.value = params;
+      tempParams = params;
       return methods;
     },
     pv: memoize((actionName: string, dependencies?: string[]) => {
@@ -83,7 +84,7 @@ export const createHandler = (commonData: any, usuallyKeys?: string[]) => {
         }
       });
     }),
-    click: (...args: Args) => core(args, { trackType: 'CLICK', optionsType: 'OPERATION' }),
+    click: (...args: Args) => core(args, { trackType: 'CLICK', optionsType: 'USER_OPERATION' }),
     do: (...args: Args) => core(args),
   };
   return methods;
